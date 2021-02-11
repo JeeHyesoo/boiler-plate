4385e808926f2578e11cbf9d8627cc5f47b78fc1
Hyesoo Jee <hyesoo.jee@gmail.com>
2021-02-11 23:26:01 +0900

    error

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -97,6 +97,8 @@ app.get('/api/users/logout',auth, (req, res)=>{
 
 
 })
+
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
